accffb5c0a10d39a11d74c2e2939fe3e2c5f8e4b
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2021-04-07 22:27:53 +0300

    TEST: project#done?

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Project do
 
     expect(project.done?).to be_truthy
   end
+
+  it 'knows that a project with an incomplete task is not done' do
+    project = Project.new
+    task = Task.new
+    project.tasks << task
+
+    expect(project.done?).to be_falsy
+  end
 end
